9871d4c99967196cad807869074e27d3a85d4eb2
alperenalasahin <120595722+alperenalasahin@users.noreply.github.com>
2023-04-23 20:18:18 +0300

    Add files via upload

---
+++ python-1.py
@@ -0,0 +1,10 @@
+import random
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunluk = int(input("sifrenin uzunlugunu girin: "))
+sifre = ""
+
+for i in range(sifre_uzunluk):
+    sifre += random.choice(karakterler)
+
+print(sifre)
